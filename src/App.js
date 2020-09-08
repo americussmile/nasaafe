6bf88a4f3a36109e59b4c9e0123579ab3c167c35
Duyen <Duyen.Huynh@kpmg.fi>
2020-09-08 19:35:23 +0300

    useState hook : does not combine value in setstate we need to use spread operator

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookcounterThree from "./components/HookCounterThree";
+import HookCounterFourth from "./components/HookCounterFour";
 
 function App() {
   return (
     <div className="App">
         <HookCounterTwo></HookCounterTwo>
         <HookcounterThree></HookcounterThree>
+        <HookCounterFourth></HookCounterFourth>
     </div>
   );
 }
